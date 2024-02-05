db8b675e7cae0aa09f4371b420fe2d6e27cc33cb
Walter Eduardo Benítez <70080918+walbtz@users.noreply.github.com>
2024-02-05 17:56:47 -0300

    Update AddUser

--- src/windows/AddUser.java
+++ src/windows/AddUser.java
@@ -60,6 +60,10 @@ public class AddUser extends javax.swing.JFrame {
         txt_phone = new javax.swing.JTextField();
         label_mail = new javax.swing.JLabel();
         txt_mail = new javax.swing.JTextField();
+        label_user = new javax.swing.JLabel();
+        txt_user = new javax.swing.JTextField();
+        label_pass = new javax.swing.JLabel();
+        txt_pass = new javax.swing.JTextField();
         label_ref = new javax.swing.JLabel();
         boton_save = new javax.swing.JButton();
         combo_rol = new javax.swing.JComboBox<>();
@@ -131,6 +135,36 @@ public class AddUser extends javax.swing.JFrame {
         });
         getContentPane().add(txt_mail, new org.netbeans.lib.awtextra.AbsoluteConstraints(349, 162, 268, 30));
 
+        label_user.setForeground(new java.awt.Color(204, 204, 204));
+        label_user.setText("USUARIO");
+        getContentPane().add(label_user, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 242, -1, -1));
+
+        txt_user.setBackground(new java.awt.Color(20, 34, 34));
+        txt_user.setFont(new java.awt.Font("Arial", 1, 16)); // NOI18N
+        txt_user.setForeground(new java.awt.Color(255, 255, 255));
+        txt_user.setBorder(null);
+        txt_user.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_userActionPerformed(evt);
+            }
+        });
+        getContentPane().add(txt_user, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 234, 268, 30));
+
+        label_pass.setForeground(new java.awt.Color(204, 204, 204));
+        label_pass.setText("CONTRASEÑA");
+        getContentPane().add(label_pass, new org.netbeans.lib.awtextra.AbsoluteConstraints(349, 242, -1, -1));
+
+        txt_pass.setBackground(new java.awt.Color(20, 34, 34));
+        txt_pass.setFont(new java.awt.Font("Arial", 1, 16)); // NOI18N
+        txt_pass.setForeground(new java.awt.Color(255, 255, 255));
+        txt_pass.setBorder(null);
+        txt_pass.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_passActionPerformed(evt);
+            }
+        });
+        getContentPane().add(txt_pass, new org.netbeans.lib.awtextra.AbsoluteConstraints(349, 234, 268, 30));
+
         label_ref.setForeground(new java.awt.Color(204, 204, 204));
         label_ref.setText("Software creado por Walter Eduardo Benítez ©");
         getContentPane().add(label_ref, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 405, -1, -1));
@@ -176,6 +210,14 @@ public class AddUser extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txt_mailActionPerformed
 
+    private void txt_userActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_userActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_userActionPerformed
+
+    private void txt_passActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_passActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_passActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -220,11 +262,15 @@ public class AddUser extends javax.swing.JFrame {
     private javax.swing.JLabel label_last_name;
     private javax.swing.JLabel label_mail;
     private javax.swing.JLabel label_name;
+    private javax.swing.JLabel label_pass;
     private javax.swing.JLabel label_phone;
     private javax.swing.JLabel label_ref;
+    private javax.swing.JLabel label_user;
     private javax.swing.JTextField txt_last_name;
     private javax.swing.JTextField txt_mail;
     private javax.swing.JTextField txt_name;
+    private javax.swing.JTextField txt_pass;
     private javax.swing.JTextField txt_phone;
+    private javax.swing.JTextField txt_user;
     // End of variables declaration//GEN-END:variables
 }
