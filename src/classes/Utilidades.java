80f556013b08d2cd1a9f81350c412dc285c94392
Walter Eduardo Benítez <70080918+walbtz@users.noreply.github.com>
2024-02-07 16:15:22 -0300

    Update

--- src/classes/Utilidades.java
+++ src/classes/Utilidades.java
@@ -11,7 +11,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
